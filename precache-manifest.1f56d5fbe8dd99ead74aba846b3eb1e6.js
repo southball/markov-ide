0d6844eda3e1c2c291889a63264f2d810a32f1dc
Jamie Choi <jamie.choi.mail.2002@gmail.com>
2020-05-07 19:19:30 +0800

    Updates

---
+++ precache-manifest.1f56d5fbe8dd99ead74aba846b3eb1e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb9c541cd69dd7a5a33bd78fee86c49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5568f38d5e9d09e8076b",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5568f38d5e9d09e8076b",
+    "url": "/static/js/2.e43564b4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e43564b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c24c023e55d60eb9af",
+    "url": "/static/js/main.13529806.chunk.js"
+  },
+  {
+    "revision": "1a4827d05bc275393d30",
+    "url": "/static/js/runtime-main.d9a2dda0.js"
+  }
+]);
